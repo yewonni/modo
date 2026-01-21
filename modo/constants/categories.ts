7e9a458a82fa893ee4d2decd4f753783fd6d2f90
ddhsl <dnjs9199@naver.com>
2026-01-21 12:29:16 +0900

    feat: add categories constants

---
+++ modo/constants/categories.ts
@@ -0,0 +1,30 @@
+export const CATEGORIES = [
+  {
+    key: "furniture",
+    label: "FURNITURE",
+    href: "/furniture",
+    icon: "/icons/furniture.png",
+    alt: "가구",
+  },
+  {
+    key: "lighting",
+    label: "LIGHTING",
+    href: "/lighting",
+    icon: "/icons/lighting.png",
+    alt: "조명",
+  },
+  {
+    key: "decor",
+    label: "DECOR",
+    href: "/decor",
+    icon: "/icons/decor.png",
+    alt: "데코",
+  },
+  {
+    key: "textile",
+    label: "TEXTILE",
+    href: "/textile",
+    icon: "/icons/textile.png",
+    alt: "패브릭",
+  },
+];
