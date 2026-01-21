8b53eaa641f58f589410396047a3af55e3e2bf4a
ddhsl <dnjs9199@naver.com>
2026-01-21 12:20:19 +0900

    chore: package update

---
+++ modo/tailwind.config.mjs
@@ -0,0 +1,16 @@
+import lineClamp from "@tailwindcss/line-clamp";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [lineClamp],
+};
+
+export default config;
