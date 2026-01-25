1b867318d7af477fd438589bdb3e14475d6c4e5e
ddhsl <dnjs9199@naver.com>
2026-01-25 21:34:42 +0900

    feat: update prisma client

--- modo/prisma/client.ts
+++ modo/prisma/client.ts
@@ -1,4 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
 export default prisma;
