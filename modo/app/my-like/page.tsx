daa92556f24992c4ff9895f54e5d65bc80cdbb7b
ddhsl <dnjs9199@naver.com>
2026-01-25 21:21:29 +0900

    refactor: split my like page components

--- modo/app/my-like/page.tsx
+++ modo/app/my-like/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { LikeButton } from "@/components/Button";
-import Pagination from "@/components/Pagination";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+import Pagination from "@/components/common/Pagination";
+import ProductCard from "@/components/my-like/ProductCard";
 
 const ALL_PRODUCTS = [
   ...Array.from({ length: 20 }).map((_, i) => ({
@@ -17,42 +16,12 @@ const ALL_PRODUCTS = [
   })),
 ];
 
-function ProductCard({ product }: { product: any }) {
-  const { store, name, price, image } = product;
-  return (
-    <li className="flex flex-col">
-      <div className="relative aspect-square w-full overflow-hidden rounded-lg">
-        <Image
-          src={image}
-          alt={name}
-          fill
-          className="object-cover transition-transform duration-300 hover:scale-105 cursor-pointer"
-        />
-      </div>
-      <div className="mt-3 flex flex-col gap-1">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <p className="cursor-pointer font-bold hover:underline max-w-22 sm:w-full text-sm lg:text-base line-clamp-1">
-              {store}
-            </p>
-            <img src="/icons/chevron-right.svg" alt="" aria-hidden />
-          </div>
-          <LikeButton />
-        </div>
-        <p className="text-sm lg:text-base line-clamp-2">{name}</p>
-        <p className="text-sm lg:text-base font-bold">{price}</p>
-      </div>
-    </li>
-  );
-}
-
 export default function MyLikePage() {
   const [currentPage, setCurrentPage] = useState(1);
-
   const itemsPerPage = 8;
   const displayedProducts = ALL_PRODUCTS.slice(
     (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
+    currentPage * itemsPerPage,
   );
 
   return (
@@ -64,7 +33,6 @@ export default function MyLikePage() {
           좋아요 ({ALL_PRODUCTS.length})
         </h3>
 
-        {/* Product List */}
         <section className="pb-24 lg:pb-34 mt-4 lg:mt-10">
           <h4 className="sr-only">상품 목록</h4>
           <ul className="grid grid-cols-2 lg:grid-cols-4 gap-6">
