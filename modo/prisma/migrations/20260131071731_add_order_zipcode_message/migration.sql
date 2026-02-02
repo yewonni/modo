ae2fd7efd969a75762cb3345e011b1c38f8e9ae8
ddhsl <dnjs9199@naver.com>
2026-02-02 12:16:36 +0900

    chore: update date base

---
+++ modo/prisma/migrations/20260131071731_add_order_zipcode_message/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `zipCode` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "message" TEXT,
+ADD COLUMN     "zipCode" TEXT NOT NULL;
