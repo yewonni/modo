8d02fbc0cf893da0ec21c121c2d9c2817b3e6022
ddhsl <dnjs9199@naver.com>
2026-02-02 12:32:34 +0900

    style: update common button component

---
+++ modo/app/components/common/Button.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { ButtonHTMLAttributes } from "react";
+import clsx from "clsx";
+
+// 기본 Button
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "primary" | "outline";
+  size?: "sm" | "md" | "lg";
+};
+
+export default function Button({
+  children,
+  variant = "primary",
+  size = "md",
+  className,
+  ...props
+}: ButtonProps) {
+  const baseStyles =
+    "cursor-pointer rounded-md font-medium leading-snug focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed";
+
+  const variantStyles = {
+    primary: "bg-primary text-white",
+    outline: "border border-[#b4b4b4]",
+  };
+
+  const hoverActiveStyles = {
+    primary:
+      "hover:bg-[#D6D1C8] hover:text-black active:bg-[#CFC8BC] active:text-black",
+    outline: "hover:bg-gray-100 active:bg-gray-200",
+  };
+
+  const sizeStyles = {
+    sm: "px-3 py-1.5 text-sm",
+    md: "px-4 py-2 text-sm",
+    lg: "px-5 py-2 text-base",
+  };
+
+  return (
+    <button
+      className={clsx(
+        baseStyles,
+        variantStyles[variant],
+        !props.disabled && hoverActiveStyles[variant],
+        sizeStyles[size],
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+// LikeButton
+type LikeButtonProps = {
+  liked: boolean;
+  onChange?: (liked: boolean) => void;
+};
+
+export function LikeButton({ liked, onChange }: LikeButtonProps) {
+  const toggle = () => {
+    onChange?.(!liked);
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={toggle}
+      className="w-8 h-8 flex items-center justify-center hover:cursor-pointer"
+    >
+      <Image
+        src={liked ? "/icons/like-full.svg" : "/icons/like-gray.svg"}
+        alt="like"
+        width={24}
+        height={24}
+      />
+    </button>
+  );
+}
+
+// StarButton
+type StarButtonProps = {
+  active?: boolean;
+  onClick?: () => void;
+};
+
+export function StarButton({ active = false, onClick }: StarButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="w-8 h-8 flex items-center justify-center hover:cursor-pointer"
+    >
+      <Image
+        src={active ? "/icons/star-on.svg" : "/icons/star-off.svg"}
+        alt="star"
+        width={24}
+        height={24}
+      />
+    </button>
+  );
+}
